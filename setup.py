a9d6b84bdaa9b2b0c1f4191e9913f7f9f59f75ce
Theresa Eimer <eimer@tnt.uni-hannover.de>
2020-10-19 15:36:50 +0200

    split dependencies for examples

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ with open("pyproject.toml", "r") as f:
 
 prod = requirements["install_requires"]
 dev = requirements["dev-dependencies"]
+examples = requirements["example-dependencies"]
 
 setup(
     name="DAClib",
     install_requires=[x for x in prod],
-    extras_require={"dev": [x for x in dev]},
+    extras_require={"dev": [x for x in dev], "example": [x for x in examples]},
 )
