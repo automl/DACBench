efb36ff5c95b756abb5481150d9b401383c65a7c
Theresa Eimer <eimer@tnt.uni-hannover.de>
2020-11-05 12:47:09 +0100

    basic modea instances

--- tests/benchmarks/test_modea_benchmark.py
+++ tests/benchmarks/test_modea_benchmark.py
@@ -23,15 +23,17 @@ class TestCMABenchmark(unittest.TestCase):
         self.assertTrue(bench.config.dummy == 0)
         os.remove("test_conf.json")
 
-    # TODO: sample meaningful instance file first
-    # def test_read_instances(self):
-    #     bench = ModeaBenchmark()
-    #     bench.read_instance_set()
-    #     self.assertTrue(len(bench.config.instance_set) == 1)
-    #     self.assertTrue(len(bench.config.instance_set[0]) == 4)
-    #     inst = bench.config.instance_set[0]
-    #     bench2 = ModeaBenchmark()
-    #     env = bench2.get_environment()
-    #     self.assertTrue(len(env.instance_set[0]) == 4)
-    #     self.assertTrue(len(env.instance_set) == 100)
-    #     self.assertTrue(inst == env.instance_set[0])
+    def test_read_instances(self):
+        bench = ModeaBenchmark()
+        bench.read_instance_set()
+        self.assertTrue(len(bench.config.instance_set) == 100)
+        inst = bench.config.instance_set[0]
+        bench2 = ModeaBenchmark()
+        env = bench2.get_environment()
+        self.assertTrue(len(env.instance_set) == 100)
+        self.assertTrue(inst == env.instance_set[0])
+
+    def test_benchmark_env(self):
+        bench = ModeaBenchmark()
+        env = bench.get_benchmark()
+        self.assertTrue(issubclass(type(env), ModeaEnv))
