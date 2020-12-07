82146089a0b6e2ef77421bd9c71f3b43db05ecac
Theresa Eimer <eimer@tnt.uni-hannover.de>
2020-12-07 16:04:28 +0100

    organize runscripts

---
+++ runscripts/run_optimal_baselines.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --partition=cpu_cluster
+#SBATCH --job-name=dacbenchlines
+#SBATCH --time=72:00:00
+#SBATCH --mem-per-cpu=8000M
+
+python ../dacbench/run_baselines.py --outdir baselines --optimal --num_episodes 1000 --benchmarks $1
