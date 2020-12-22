c17d76d3ca1acb7791513060dd112d84b3db712f
Rishan <rd.senanayake@gmail.com>
2020-12-22 01:02:24 +0100

    Add sgd benchmark

--- dacbench/benchmarks/__init__.py
+++ dacbench/benchmarks/__init__.py
@@ -3,6 +3,7 @@ from dacbench.benchmarks.sigmoid_benchmark import SigmoidBenchmark
 from dacbench.benchmarks.fast_downward_benchmark import FastDownwardBenchmark
 from dacbench.benchmarks.cma_benchmark import CMAESBenchmark
 from dacbench.benchmarks.modea_benchmark import ModeaBenchmark
+from dacbench.benchmarks.sgd_benchmark import SGDBenchmark
 
 __all__ = [
     "LubyBenchmark",
@@ -10,4 +11,5 @@ __all__ = [
     "FastDownwardBenchmark",
     "CMAESBenchmark",
     "ModeaBenchmark",
+    "SGDBenchmark",
 ]
