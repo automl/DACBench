cbd17df11a33ec689dce459d1b615467a42d6583
Maximilian Reimer <maximilian.reimer.42@gmail.com>
2020-12-29 14:01:20 +0100

    Implemented automatically updating of step and episode

--- dacbench/logger.py
+++ dacbench/logger.py
@@ -1,29 +1,166 @@
 import json
+from abc import ABCMeta, abstractmethod
 from collections import defaultdict
 from datetime import datetime
+from functools import reduce
 from itertools import chain
 from pathlib import Path
+import pandas as pd
 from typing import Union, Dict, Any, Tuple, List
 
+from gym import Wrapper
 
-class Logger:
+from dacbench import AbstractEnv
+from dacbench.abstract_agent import AbstractDACBenchAgent
+
+
+def split(predicate, iterable) -> Tuple[List, List]:
+    """
+    Splits the iterable into two list depending on the result of predicate.
+    :param predicate:
+    :param iterable:
+    :return: positives, negatives
+    """
+    positives, negatives = [], []
+
+    for item in iterable:
+        (positives if predicate(item) else negatives).append(item)
+
+    return positives, negatives
+
+
+def flatten_log_entry(log_entry: Dict) -> List[Dict]:
+    """
+    Transforms a log entry of format like
+    {
+        'step': 0,
+        'episode': 2,
+        'some_value': {
+            'values' : [34, 45],
+            'times':['28-12-20 16:20:53', '28-12-20 16:21:30'],
+        }
+    }
+    into
+    [
+        { 'step': 0,'episode': 2, 'value': 34, 'time': '28-12-20 16:20:53'},
+        { 'step': 0,'episode': 2, 'value': 45, 'time': '28-12-20 16:21:30'}
+    ]
+
+    :param log_entry:
+    :return:
+    """
+    dict_entries, top_level_entries = split(
+        lambda item: isinstance(item[1], dict), log_entry.items()
+    )
+    rows = []
+    for value_name, value_dict in dict_entries:
+        current_rows = (
+            dict(
+                top_level_entries
+                + [("value", value), ("time", time), ("name", value_name)]
+            )
+            for value, time in zip(value_dict["values"], value_dict["times"])
+        )
+
+        rows.extend(map(dict, current_rows))
+
+    return rows
+
+
+def log2dataframe(logs: List[dict], wide=False) -> pd.DataFrame:
+    """
+    Converts a list of log entries to a pandas dataframe.
+    :param logs:
+    :param wide: determines the format of the dataframe.
+    wide=False (default) produces a dataframe with columns (episode, step, time, name, value)
+    wide=True returns a dataframe (episode, step, time, name_1, name_2, ...) if the variable name_n has not been logged
+    at (episode, step, time) name_n is NaN.
+    :return:
+    """
+    flat_logs = map(flatten_log_entry, logs)
+    rows = reduce(lambda l1, l2: l1 + l2, flat_logs)
+
+    dataframe = pd.DataFrame(rows)
+    if wide:
+        index_columns = ["episode", "step", "time", "name"]
+        dataframe = dataframe.set_index(index_columns)
+        dataframe = dataframe.unstack()
+    return dataframe
+
+
+class AbstractLogger(metaclass=ABCMeta):
     valid_types = {
         "recursive": [dict, list, tuple],
         "primitive": [str, int, float, bool],
     }
 
-    def __pretty_valid_types(self) -> str:
+    def __init__(self, experiment_name: str, output_path: Path):
+        self.experiment_name = experiment_name
+        self.output_path = output_path
+        self.log_dir = self._init_logging_dir(self.output_path / self.experiment_name)
+
+    def _pretty_valid_types(self) -> str:
         valid_types = chain(
             self.valid_types["recursive"], self.valid_types["primitive"]
         )
-        return ", ".join(map(lambda type: type.__name__, valid_types))
+        return ", ".join(map(lambda type_: type_.__name__, valid_types))
 
+    @staticmethod
+    def _init_logging_dir(log_dir: Path):
+        log_dir.mkdir(parents=True, exist_ok=True)
+        return log_dir
+
+    def is_of_valid_type(self, value: Any) -> bool:
+        f"""
+        Check if the value of any type in {self._pretty_valid_types()}
+        :param value:
+        :return:
+        """
+        if any(isinstance(value, type) for type in self.valid_types["primitive"]):
+            return True
+
+        elif any(isinstance(value, type) for type in self.valid_types["recursive"]):
+            value = value.keys() if isinstance(value, dict) else value
+            return all(self.is_of_valid_type(sub_value) for sub_value in value)
+
+        else:
+            return False
+
+    @abstractmethod
+    def close(self):
+        pass
+
+    @abstractmethod
+    def next_step(self):
+        pass
+
+    @abstractmethod
+    def next_episode(self):
+        pass
+
+    @abstractmethod
+    def write(self):
+        pass
+
+    @abstractmethod
+    def log(self, key, value):
+        f"""
+        Write value to list of values for key.
+        :param key:
+        :param value: the value must of of a type that is
+        json serializable. Currently only {self._pretty_valid_types()} and recursive types of these are
+        valid
+        :return:
+        """
+        pass
+
+
+class ModuleLogger(AbstractLogger):
     def __init__(
         self,
         output_path: Path,
         experiment_name: str,
-        module: str = None,
-        write_every_n_steps: int = 1,
+        module: str,
     ) -> None:
         """
         All results are placed under 'output_path / experiment_name'
@@ -31,13 +168,12 @@ class Logger:
         :param experiment_name: name of the experiment.
         :param the module (mostly name of the wrapper), each wrapper get's its own file
         """
+        super(ModuleLogger, self).__init__(experiment_name, output_path)
         # todo write buffer every frequency
         # todo add multi module support
         # todo add registration
-        self.output_path = output_path
-        self.experiment_name = experiment_name
-        self.log_dir = self.__init_logging_dir(self.output_path / self.experiment_name)
-        self.log_file = open(self.log_dir / "{module}.jsonl", "a+")
+
+        self.log_file = open(self.log_dir / f"{module}.jsonl", "a+")
 
         self.step = 0
         self.episode = 0
@@ -61,11 +197,6 @@ class Logger:
     def __init_dict():
         return defaultdict(lambda: {"times": [], "values": []})
 
-    @staticmethod
-    def __init_logging_dir(log_dir: Path):
-        log_dir.mkdir(parents=True, exist_ok=True)
-        return log_dir
-
     def __reset_episode(self):
         self.__end_step()
         self.episode = 0
@@ -88,23 +219,9 @@ class Logger:
 
     def __buffer_to_file(self):
         self.log_file.write("\n".join(self.buffer))
+        self.log_file.write("\n")
         self.buffer.clear()
-
-    def is_of_valid_type(self, value: Any) -> bool:
-        f"""
-        Check if the value of any type in {self.__pretty_valid_types()}
-        :param value:
-        :return:
-        """
-        if any(isinstance(value, type) for type in self.valid_types["primitive"]):
-            return True
-
-        elif any(isinstance(value, type) for type in self.valid_types["recursive"]):
-            value = value.keys() if isinstance(value, dict) else value
-            return all(self.is_of_valid_type(sub_value) for sub_value in value)
-
-        else:
-            return False
+        self.log_file.flush()
 
     def log(
         self, key: str, value: Union[Dict, List, Tuple, str, int, float, bool]
@@ -113,14 +230,14 @@ class Logger:
         Write value to list of values for key.
         :param key:
         :param value: the value must of of a type that is
-        json serializable. Currently only {self.__pretty_valid_types()} and recursive types of these are
+        json serializable. Currently only {self._pretty_valid_types()} and recursive types of these are
         valid
         :return:
         """
         # TODO add numpy support
         # TODO add instance and benchmark
         if not self.is_of_valid_type(value):
-            valid_types = self.__pretty_valid_types()
+            valid_types = self._pretty_valid_types()
             raise ValueError(
                 f"value {type(value)} is not of valid type or a recursive composition of valid types ({valid_types})"
             )
@@ -128,3 +245,96 @@ class Logger:
             datetime.now().strftime("%d-%m-%y %H:%M:%S")
         )
         self.current_step[key]["values"].append(value)
+
+
+class Logger(AbstractLogger):
+    def __init__(self, experiment_name: str, output_path: Path) -> None:
+        """
+
+        :param experiment_name:
+        :param output_path:
+        """
+        super(Logger, self).__init__(experiment_name, output_path)
+        self.module_logger: Dict[str, ModuleLogger] = dict()
+
+    def close(self):
+        for _, module_logger in self.module_logger.items():
+            module_logger.close()
+
+    def __del__(self):
+        self.close()
+
+    def next_step(self):
+        for _, module_logger in self.module_logger.items():
+            module_logger.next_step()
+
+    def next_episode(self):
+        for _, module_logger in self.module_logger.items():
+            module_logger.next_episode()
+
+    def write(self):
+        for _, module_logger in self.module_logger.items():
+            module_logger.write()
+
+    def add_module(self, module: str) -> ModuleLogger:
+        if module in self.module_logger:
+            raise ValueError(f"Module {module} already registered")
+        else:
+            self.module_logger[module] = ModuleLogger(
+                self.output_path, self.experiment_name, module
+            )
+
+        return self.module_logger[module]
+
+    @staticmethod
+    def __get_env(env: Union[Wrapper, AbstractEnv]) -> AbstractEnv:
+        while hasattr(env, "env"):
+            env = env.env
+        return env
+
+    def add_env(self, env: Union[Wrapper, AbstractEnv]) -> AbstractLogger:
+        """
+        Registers the env and if it is a wrapper recursively the other wrappers as well as the env.
+        From now on all log(...) calls of the on the env or the wrapper will write to the values to a jsonl file.
+        :param env:
+        :return:
+        """
+        # todo for all wrappers recursively?
+
+        logger = self.add_module(env)
+
+        # wrap step and reset of underling env
+        real_env = self.__get_env(env)
+
+        def step(*args, **kwargs):
+            self.next_step()
+            next_state, reward, done, info = real_env.original_step(*args, **kwargs)
+            # if done:
+            #    self.next_episode()
+            return next_state, reward, done, info
+
+        real_env.original_step = real_env.step
+        real_env.step = step
+
+        def reset(*args, **kwargs):
+            self.next_episode()
+            return real_env.original_reset(*args, **kwargs)
+
+        real_env.original_reset = real_env.reset
+        real_env.reset = reset
+
+        return logger
+
+    def add_agent(self, agent: AbstractDACBenchAgent):
+        """
+        Registries the agent
+        :param agent:
+        :return:
+        """
+        # todo implement
+        raise NotImplementedError()
+
+    def log(self, key, value, module):
+        if module not in self.module_logger:
+            raise ValueError(f"Module {module} not registered yet")
+        self.module_logger.log(key, value)
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -2,15 +2,70 @@ import json
 import unittest
 from pathlib import Path
 import tempfile
-from dacbench.logger import Logger
+
+from dacbench.agents.simple_agents import RandomAgent
+from dacbench.benchmarks import SigmoidBenchmark
+from dacbench.logger import ModuleLogger, Logger
 
 
 class TestLogger(unittest.TestCase):
-    def test_basic_logging(self):
+    def test_wrapper(self):
+        episodes = 3
+        # todo instances
         temp_dir = tempfile.TemporaryDirectory()
         experiment_name = "test_experiment"
         logger = Logger(
             output_path=Path(temp_dir.name), experiment_name=experiment_name
+        )
+
+        benchmark = SigmoidBenchmark()
+        env = benchmark.get_benchmark()
+        agent = RandomAgent(env)
+
+        env_logger = logger.add_env(env)
+
+        for episode in range(1, episodes + 1):
+            state = env.reset()
+            done = False
+            reward = 0
+            step = 0
+            while not done:
+                action = agent.act(state, reward)
+                env_logger.log("logged_step", step)
+                env_logger.log("logged_episode", episode)
+                next_state, reward, done, _ = env.step(action)
+                env_logger.log("reward", reward)
+                env_logger.log("done", done)
+                agent.train(next_state, reward)
+                state = next_state
+
+                step += 1
+            agent.end_episode(state, reward)
+            logger.write()
+
+        env.close()
+        logger.close()
+
+        with open(env_logger.log_file.name, "r") as log_file:
+            logs = list(map(json.loads, log_file))
+
+        for log in logs:
+            if "logged_step" in log:
+                self.assertEqual(log["logged_step"]["values"][0], log["step"])
+            if "logged_episode" in log:
+                self.assertEqual(log["logged_episode"]["values"][0], log["episode"])
+        temp_dir.cleanup()
+
+
+class TestModuleLogger(unittest.TestCase):
+    def test_basic_logging(self):
+        temp_dir = tempfile.TemporaryDirectory()
+        experiment_name = "test_experiment"
+        module_name = "module"
+        logger = ModuleLogger(
+            output_path=Path(temp_dir.name),
+            experiment_name=experiment_name,
+            module=module_name,
         )
 
         for episode in range(10):
@@ -21,25 +76,24 @@ class TestLogger(unittest.TestCase):
             logger.next_episode()
         logger.write()
 
-        logger.log_file.seek(0)
-        logs = logger.log_file.read().splitlines()
+        with open(logger.log_file.name, "r") as log_file:
+            logs = list(map(json.loads, log_file))
         self.assertEqual(
             10 * 3, len(logs), "For each step with logging done in it one line exit"
         )
-        for line in logs:
-            log_step = json.loads(line)
+        for log in logs:
             self.assertTrue(
                 all(
-                    log_step["episode"] == logged_episode
-                    for logged_episode in log_step.get(
-                        "episode_logged", {"values": []}
-                    )["values"]
+                    log["episode"] == logged_episode
+                    for logged_episode in log.get("episode_logged", {"values": []})[
+                        "values"
+                    ]
                 )
             )
             self.assertTrue(
                 all(
-                    log_step["step"] == logged_episode
-                    for logged_episode in log_step.get("step_logged", {"values": []})[
+                    log["step"] == logged_episode
+                    for logged_episode in log.get("step_logged", {"values": []})[
                         "values"
                     ]
                 )
