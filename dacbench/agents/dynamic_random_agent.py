e6b2d370c917c5c125377abbdb77a93d86aaa965
Theresa Eimer <eimer@tnt.uni-hannover.de>
2021-01-04 16:05:26 +0100

    Random agent with fixed switching intervals

---
+++ dacbench/agents/dynamic_random_agent.py
@@ -0,0 +1,17 @@
+class DynamicRandomAgent(AbstractDACBenchAgent):
+    def __init__(self, env, switching_interval):
+        self.sample_action = env.action_space.sample
+        self.switching_interval = switching_interval
+        self.count = 0
+        self.action = self.sample_action()
+
+    def act(self, state, reward):
+        if self.count >= self.switching_interval:
+            self.action = self.sample_action()
+        return self.action
+
+    def train(self, next_state, reward):
+        pass
+
+    def end_episode(self, state, reward):
+        pass
