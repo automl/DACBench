6304abb61f3656543bc6c25af9cd685eaeab4782
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2021-07-09 16:34:18 +0300

    add option to generate config file seperately #99

--- tests/envs/generate_sgd_tests.py
+++ tests/envs/generate_sgd_tests.py
@@ -1,3 +1,6 @@
+"""
+This script can be used to generate test cases for SGD benchmark. Config file is saved seperately and then it is used to generate test cases so that config file can be regenerated after a refactoring without touching the test case files.
+"""
 import argparse
 import os
 import json
@@ -11,22 +14,32 @@ from dacbench.benchmarks import SGDBenchmark
 from dacbench.wrappers import ObservationWrapper
 
 parser = argparse.ArgumentParser()
-parser.add_argument('type', choices=['static', 'dynamic'])
+parser.add_argument('type', choices=['config', 'static', 'dynamic'])
 args = parser.parse_args()
 
-SEED = 123
-np.random.seed(SEED)
 benchmark = SGDBenchmark()
-benchmark.config = objdict(SGD_DEFAULTS.copy())
-benchmark.config.seed = SEED
-benchmark.read_instance_set()
-env = SGDEnv(benchmark.config)
-env = ObservationWrapper(env)
-
+SEED = 123
 data_path = os.path.join(os.path.dirname(__file__), 'data')
 
-with open(os.path.join(data_path, 'sgd_benchmark_config.pickle'), 'wb') as f:
-    pickle.dump(benchmark.config, f)
+if args.type == 'config':
+    with open(os.path.join(data_path, 'sgd_benchmark_config.pickle'), 'wb') as f:
+        np.random.seed(SEED)
+        benchmark.config = objdict(SGD_DEFAULTS.copy())
+        benchmark.config.seed = SEED
+        benchmark.read_instance_set()
+        pickle.dump(benchmark.config, f)
+        exit(0)
+
+config_path = os.path.join(data_path, 'sgd_benchmark_config.pickle')
+if not os.path.exists(config_path):
+    raise FileNotFoundError('''"sgd_benchmark_config.pickle" is not found.\
+ Call "python generate_sgd_tests.py config" to generate it.''')
+
+with open(config_path, 'rb') as f:
+    benchmark.config = pickle.load(f)
+
+env = SGDEnv(benchmark.config)
+env = ObservationWrapper(env)
 
 env.reset()
 done = False
