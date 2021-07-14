69fc4242c5b97e2a7eab0485aa6a39b5d8779729
Rasmus <rasmusvong@gmx.de>
2021-07-14 15:38:27 +0200

    feat: initial work

--- dacbench/envs/geometric.py
+++ dacbench/envs/geometric.py
@@ -3,6 +3,7 @@ Geometric environment.
 Original environment authors: Rasmus von Glahn
 """
 import numpy as np
+import itertools
 from typing import List
 
 from dacbench import AbstractEnv
@@ -10,28 +11,39 @@ from dacbench import AbstractEnv
 
 class GeometricEnv(AbstractEnv):
     """
-    Environment for tracing difernet curves that are orthogonal to each other
-    Use product approach: f(x,y,z) = X(x) * Y(y) * Z(z)
+    Environment for tracing different curves that are orthogonal to each other
+    Use product approach: f(t,x,y,z) = X(t,x) * Y(t,y) * Z(t,z)
+    Normalize Function Value on a Scale betwwen 0 and 1
+        - min and max value for normalization over all timesteps
     """
 
-    def _sig(self, x, scaling, inflection):
+    def _sigmoid(self, t: float, scaling: float, inflection: float):
         """ Simple sigmoid function """
-        return 1 / (1 + np.exp(-scaling * (x - inflection)))
+        return 1 / (1 + np.exp(-scaling * (t - inflection)))
 
-    def _linear(self, x: float, a: float, b: float):
+    def _linear(self, t: float, a: float, b: float):
         """ Linear function """
-        return a * x + b
+        return a * t + b
 
-    def _polynom(self, x: float, coeff_list: List[float]):
+    def _polynom(self, t: float, coeff_list: List[float]):
         """ Polynomial function. Dimension depends on length of coefficient list. """
         pol_value = 0
 
         for dim, coeff in enumerate(coeff_list):
-            pol_value += coeff * x ** dim
+            pol_value += coeff * t ** dim
 
         return pol_value
 
+    def _logarithmic():
+        """ Logarithmic function """
+        pass
+
+    def _exponential():
+        """ Exponential function """
+        pass
+
     def _constant(self, c: float):
+        """ Constant function """
         return c
 
     def __init__(self, config) -> None:
@@ -43,7 +55,33 @@ class GeometricEnv(AbstractEnv):
         config : objdict
             Environment configuration
         """
-        pass
+        super(GeometricEnv, self).__init__(config)
+
+        self.action_vals = config["action_values"]
+        self.n_actions = len(self.action_vals)
+        self.action_mapper = {}
+        self.function_stack = []
+        self.function_params = []
+
+        # Map actions to differnet actions to differnet action configurations
+        for idx, prod_idx in zip(
+            range(np.prod(config["action_values"])),
+            itertools.product(*[np.arange(val) for val in config["action_values"]]),
+        ):
+            self.action_mapper[idx] = prod_idx
+
+        self._prev_state = None
+        self.action = None
+
+        if "reward_function" in config.keys():
+            self.get_reward = config["reward_function"]
+        else:
+            self.get_reward = self.get_default_reward
+
+        if "state_method" in config.keys():
+            self.get_state = config["state_method"]
+        else:
+            self.get_state = self.get_default_state
 
     def step(self, action: int):
         """
@@ -59,7 +97,17 @@ class GeometricEnv(AbstractEnv):
         np.array, float, bool, dict
             state, reward, done, info
         """
-        pass
+        self.done = super(GeometricEnv, self).step_()
+
+        action = self.action_mapper[action]
+        assert self.n_actions == len(
+            action
+        ), f"action should be of length {self.n_actions}."
+        self.action = action
+
+        next_state = self.get_state(self)
+        self._prev_state = next_state
+        return next_state, self.get_reward(self), self.done, {}
 
     def reset(self) -> List[int]:
         """
@@ -70,9 +118,10 @@ class GeometricEnv(AbstractEnv):
         numpy.array
             Environment state
         """
-        pass
+        super(GeometricEnv, self).reset_()
 
     def get_default_reward(self, _):
+        # TODO: calculate euklidean distance between curve and actions
         pass
 
     def get_default_state(self, _):
@@ -87,7 +136,7 @@ class GeometricEnv(AbstractEnv):
         bool
             Closing confirmation
         """
-        pass
+        True
 
     def render(self, mode: str) -> None:
         """
