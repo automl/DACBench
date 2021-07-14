f5a0eff946cbf1cc6cf9a1fd4ece205b8f976cc6
Rasmus <rasmusvong@gmx.de>
2021-07-14 15:37:43 +0200

    feat: add read_instance_set

--- dacbench/benchmarks/geometric_benchmark.py
+++ dacbench/benchmarks/geometric_benchmark.py
@@ -1,3 +1,4 @@
+from dacbench.benchmarks.sigmoid_benchmark import FILE_PATH
 from dacbench.abstract_benchmark import AbstractBenchmark, objdict
 from dacbench.envs import GeometricEnv
 
@@ -6,7 +7,7 @@ import os
 import csv
 
 
-ACTION_VALUES = (5, 10)
+ACTION_VALUES = (3, 3)
 
 
 INFO = {
@@ -37,9 +38,18 @@ GEOMETRIC_DEFAULTS = objdict(
         "reward_range": (0, 1),
         "cutoff": 10,
         "action_values": ACTION_VALUES,
-        "seed": 0,
+        "action" "seed": 0,
+        "variable_action_value": False,
         "instance_set_path": "../instance_sets/geometric/geometric_train.csv",
         "benchmark_info": INFO,
+    }
+)
+
+# apply if "variable_action_value" is set to True
+ACTION_VALUE_MAPPING = objdict(
+    {
+        "sigmoid:": 5,
+        "constant": 2,
     }
 )
 
@@ -86,36 +96,68 @@ class GeometricBenchmark(AbstractBenchmark):
         return env
 
     def read_instance_set(self):
-        """Read instance set from file"""
-        path = (
-            os.path.dirname(os.path.abspath(__file__))
-            + "/"
-            + self.config.instance_set_path
-        )
+        """
+        Read instance set from file
+        Creates a nested List for every Intance.
+        The List contains all functions with their respective values.
+        """
+        path = os.path.join(FILE_PATH, self.config.instance_set_path)
         self.config["instance_set"] = {}
         with open(path, "r") as fh:
+
+            known_ids = []
             reader = csv.DictReader(fh)
+
             for row in reader:
-                # TODO: - first create instance set for different configurations
-                pass
+                function_list = []
+
+                if row["ID"] not in known_ids:
+                    self.config.instance_set[row["ID"]] = []
+                    known_ids.append(row["ID"])
+
+                for index, element in enumerate(row.values()):
+
+                    if element == "0" and index != 0:
+                        break
+
+                    function_list.append(element)
+
+                self.config.instance_set[row["ID"]].append(function_list)
 
     def get_benchmark(self, dimension=None, seed=0):
-        self.config = objdict(GEOMETRIC_DEFAULTS.copy())
-        # TODO: wie wird instance_set generiert? Dynamisch statisch? Welche Vorgaben? Generator oder json?
+        """
+        [summary]
 
-        if dimension == 1:
-            self._set_action_values([3])
-            self.config.instance_set_path = (
-                "../instance_sets/geometric/geometric_XY_train.csv"
-            )
-            self.config.benchmark_info["state_description"] = [
-                "Remaining Budget",
-                "tba",
-                "Action",
-            ]
+        Parameters
+        ----------
+        dimension : [type], optional
+            [description], by default None
+        seed : int, optional
+            [description], by default 0
+
+        Returns
+        -------
+        [type]
+            [description]
+        """
+        self.config = objdict(GEOMETRIC_DEFAULTS.copy())
+
+        # Call generator from here, if data not available
+
+        self.config.instance_set_path = "../instance_sets/geometric/geometric_train.csv"
+        self.config.benchmark_info["state_description"] = [
+            "Remaining Budget",
+            "Function List",
+            "Action",
+        ]
 
         self.config.seed = seed
         self.read_instance_set()
+        # TODO: Set valid action values
+        #   - depend on isntance set
+        #   - depend on discretization level of functions
+        self._set_action_values([3])
+
         env = GeometricEnv(self.config)
         return env
 
@@ -134,3 +176,8 @@ class GeometricBenchmark(AbstractBenchmark):
             np.array([-np.inf for _ in range(1 + len(values) * 3)]),
             np.array([np.inf for _ in range(1 + len(values) * 3)]),
         ]
+
+
+if __name__ == "__main__":
+    geo_env = GeometricBenchmark()
+    geo_env.read_instance_set()
