adb880c6f621b21f17eec8b09deebb046916af5a
Rasmus <rasmusvong@gmx.de>
2021-07-25 17:39:16 +0200

    tests: fix tests and handle class variable in dict

--- tests/benchmarks/test_geometric_benchmark.py
+++ tests/benchmarks/test_geometric_benchmark.py
@@ -13,13 +13,16 @@ from dacbench.envs import GeometricEnv
 DEFAULTS_DYNAMIC = objdict(
     {
         "action_space_class": "Discrete",
-        "action_space_args": [],
+        "action_space_args": [1],
         "observation_space_class": "Box",
-        "observation_space_type": np.float32,
-        "observation_space_args": [],
+        "observation_space_type": None,
+        "observation_space_args": [
+            1,
+            1,
+        ],
         "reward_range": (0, 1),
         "cutoff": 10,
-        "action_values": [],
+        "action_values": [1],
         "action_value_default": 4,
         "action_values_variable": True,  # if True action value mapping will be used
         "action_value_mapping": {  # defines number of action values for differnet functions
@@ -30,11 +33,13 @@ DEFAULTS_DYNAMIC = objdict(
             "polynomial7D": 11,
             "exponential": 4,
             "logarithmic": 4,
+            "constant": 1,
         },
         "action_interval_mapping": {},  # maps actions to equally sized intervalls in [-1, 1]
         "seed": 0,
         "max_function_value": 10000,  # clip function value if it is higher than this number
         "instance_set_path": "../instance_sets/geometric/geometric_unit_test.csv",
+        "benchmark_info": "Hallo",
     }
 )
 
@@ -42,30 +47,34 @@ DEFAULTS_DYNAMIC = objdict(
 DEFAULTS_STATIC = objdict(
     {
         "action_space_class": "Discrete",
-        "action_space_args": [],
+        "action_space_args": [1],
         "observation_space_class": "Box",
-        "observation_space_type": np.float32,
-        "observation_space_args": [],
+        "observation_space_type": None,
+        "observation_space_args": [
+            1,
+            1,
+        ],
         "reward_range": (0, 1),
         "cutoff": 10,
-        "action_values": [],
+        "action_values": [1],
         "action_value_default": 4,
         "action_values_variable": False,  # if True action value mapping will be used
         "action_interval_mapping": {},  # maps actions to equally sized intervalls in [-1, 1]
         "max_function_value": 10000,  # clip function value if it is higher than this number
         "instance_set_path": "../instance_sets/geometric/geometric_unit_test.csv",
+        "benchmark_info": "Hallo",
     }
 )
 
 
-class TestSigmoidBenchmark(unittest.TestCase):
-    def load_bench(self, config: Dict):
-        with open("test_config.json", "w+") as fp:
+class TestGeometricBenchmark(unittest.TestCase):
+    def load_bench(self, config):
+        with open("data.json", "w") as fp:
             json.dump(config, fp)
 
-        absolute_path = os.path.abspath("test_conf.json")
-        bench = GeometricBenchmark(absolute_path)
-        os.remove("test_config.json")
+        absolute_path = os.path.abspath("data.json")
+        bench = GeometricBenchmark(config_path=absolute_path)
+        os.remove("data.json")
         return bench
 
     def test_get_env(self):
@@ -76,7 +85,6 @@ class TestSigmoidBenchmark(unittest.TestCase):
     def test_setup(self):
         bench = GeometricBenchmark()
         self.assertTrue(bench.config is not None)
-
         config = {"dummy": 0}
         with open("test_conf.json", "w+") as fp:
             json.dump(config, fp)
@@ -85,28 +93,33 @@ class TestSigmoidBenchmark(unittest.TestCase):
         os.remove("test_conf.json")
 
     def test_save_conf(self):
-        bench = GeometricBenchmark()
-        bench.save_config("test_conf.json")
-        with open("test_conf.json", "r") as fp:
+        bench = self.load_bench(DEFAULTS_STATIC)
+        bench.read_instance_set()
+        bench.set_action_values()
+
+        absolute_path = os.path.abspath("data.json")
+        bench.save_config(absolute_path)
+        with open(absolute_path, "r") as fp:
             recovered = json.load(fp)
+
+        del bench.config["instance_set"]
         for k in bench.config.keys():
             self.assertTrue(k in recovered.keys())
-        os.remove("test_conf.json")
+        os.remove("data.json")
 
     def test_read_instance_set(self):
         bench = self.load_bench(DEFAULTS_STATIC)
         bench.read_instance_set()
         self.assertTrue(len(bench.config.instance_set.keys()) == 100)
-        self.assertTrue(len(bench.config.instance_set[0]) == 7)
-        self.assertTrue(len(bench.config.instance_set[0][0][1]) == "sigmoid")
-
-        first_inst = bench.config.instance_set[0]
+        self.assertTrue(len(bench.config.instance_set[0]) == 8)
+        self.assertTrue(bench.config.instance_set[0][0][1] == "sigmoid")
+        first_inst = bench.config.instance_set[0][0][3]
 
         bench2 = self.load_bench(DEFAULTS_STATIC)
         env = bench2.get_environment()
-        self.assertTrue(env.instance_set[0] == first_inst)
+        self.assertTrue(env.instance_set[0][0][3] == first_inst)
         self.assertTrue(len(env.instance_set.keys()) == 100)
-        self.assertTrue(len(bench.config.instance_set[0][0][1]) == "linear")
+        self.assertTrue(bench.config.instance_set[0][1][1] == "linear")
 
     def test_benchmark_env(self):
         bench = GeometricBenchmark()
@@ -116,11 +129,11 @@ class TestSigmoidBenchmark(unittest.TestCase):
     def test_set_action_values_static(self):
         bench = self.load_bench(DEFAULTS_STATIC)
         bench.read_instance_set()
-        bench._set_action_values()
+        bench.set_action_values()
         self.assertTrue(bench.config.action_value_default == 4)
         self.assertTrue(bench.config.action_values[0] == 4)
-        self.assertTrue(bench.config.action_space_args == [16384])
-        self.assertTrue(len(bench.config.observation_space_args[0]) == 29)
+        self.assertTrue(bench.config.action_space_args[0] == 65536)
+        self.assertTrue(len(bench.config.observation_space_args[0]) == 33)
         self.assertTrue(
             len(bench.config.action_interval_mapping)
             == len(bench.config.action_value_mapping)
@@ -129,8 +142,8 @@ class TestSigmoidBenchmark(unittest.TestCase):
     def test_set_action_values_dynamic(self):
         bench = self.load_bench(DEFAULTS_DYNAMIC)
         bench.read_instance_set()
-        bench._set_action_values()
+        bench.set_action_values()
         self.assertTrue(bench.config.action_values_variable == True)
         self.assertTrue(bench.config.action_values[4] == 11)
-        self.assertTrue(bench.config.action_space_args == [55440])
-        self.assertTrue(len(bench.config.observation_space_args[0]) == 38)
+        self.assertTrue(bench.config.action_space_args[0] == 55440)
+        self.assertTrue(len(bench.config.observation_space_args[0]) == 39)
