e290bfd5ec9ef6e86926fdd0f9cddd9795e8fb86
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2021-09-19 14:35:06 +0300

    import onell

--- dacbench/benchmarks/__init__.py
+++ dacbench/benchmarks/__init__.py
@@ -4,6 +4,7 @@ from dacbench.benchmarks.fast_downward_benchmark import FastDownwardBenchmark
 from dacbench.benchmarks.cma_benchmark import CMAESBenchmark
 from dacbench.benchmarks.modea_benchmark import ModeaBenchmark
 from dacbench.benchmarks.sgd_benchmark import SGDBenchmark
+from dacbench.benchmarks.onell_benchmark import OneLLBenchmark
 from dacbench.benchmarks.toysgd_benchmark import ToySGDBenchmark
 
 __all__ = [
