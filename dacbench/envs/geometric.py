d095be25b12bade802d4859317d165943b148018
Rasmus von Glahn <rasmusvong@gmx.de>
2021-10-08 17:07:51 +0200

    refactor: add functions class

--- dacbench/envs/geometric.py
+++ dacbench/envs/geometric.py
@@ -45,35 +45,34 @@ class GeometricEnv(AbstractEnv):
         self.max_function_value = config["max_function_value"]
         self.realistic_trajectory = config["realistic_trajectory"]
         self.derivative_interval = config["derivative_interval"]
-        self.correlation_table = config["correlation_table"]
-        self.correlation_active = True if config["correlation_table"] else False
-        self.correlation_depth = 4
 
+        self.correlation_table = config["correlation_table"]
+        self.correlation_active = config["create_correlation"]
+        self.correlation_depth = config["correlation_depth"]
+
+        self._prev_state = None
+        self.action = None
         self.n_actions = len(self.action_vals)
         self.action_mapper = {}
 
-        # state variables
-        self.trajectory = []  # multi dimensional representation of action space
-        self.agent_trajectory = []  # one dimensional representation of action space
+        # Function object
+        self.functions = Functions(self.n_steps, self.n_actions)
+
+        # Trajectories
+        self.action_trajectory = []
         self.coord_trajectory = []
-        self.trajectory_set = {}
+        self.action_trajectory_set = {}
         self.coord_trajectory_set = {}
 
         self.derivative = []
         self.derivative_set = {}
 
-        # map actions from int to vector representation
+        # Map actions from int to vector representation
         for idx, prod_idx in zip(
             range(np.prod(config["action_values"])),
             itertools.product(*[np.arange(val) for val in config["action_values"]]),
         ):
             self.action_mapper[idx] = prod_idx
-
-        self._prev_state = None
-        self.action = None
-
-        self._calculate_norm = True
-        self._calculate_norm_value()
 
         if "reward_function" in config.keys():
             self.get_reward = config["reward_function"]
@@ -84,33 +83,6 @@ class GeometricEnv(AbstractEnv):
             self.get_state = config["state_method"]
         else:
             self.get_state = self.get_default_state
-
-    def get_coordinates(self, instance: List = None) -> List[np.array]:
-        """
-        Calculates coordinates for instance over all time_steps
-
-        Parameters
-        ----------
-        instance : List, optional
-            Instance that holds information about functions, by default None
-
-        Returns
-        -------
-        List[np.array]
-            Index of List refers to time step
-        """
-        if not instance:
-            instance = self.instance
-
-        optimal_policy_coord = np.zeros((self.n_steps, self.n_actions))
-
-        for time_step in range(self.n_steps):
-            # TODO: apply correlation
-            optimal_policy_coord[time_step, :] = self._get_optimal_policy_at_time_step(
-                instance, time_step
-            )
-
-        return optimal_policy_coord
 
     def get_optimal_policy(
         self, instance: List = None, vector_action: bool = True
@@ -133,7 +105,7 @@ class GeometricEnv(AbstractEnv):
         if not instance:
             instance = self.instance
 
-        optimal_policy_coords = self.get_coordinates(instance)
+        optimal_policy_coords = self.functions.get_coordinates(instance)
         optimal_policy = np.zeros(((self.n_steps, self.n_actions)))
 
         for step in range(self.n_steps):
@@ -177,19 +149,17 @@ class GeometricEnv(AbstractEnv):
         ), f"action should be of length {self.n_actions}."
         self.action = action_vec
 
-        self.trajectory.append(np.array(action_vec))
-        self.agent_trajectory.append(np.array(action))
-        self.coord_trajectory.append(
-            self._get_optimal_policy_at_time_step(self.instance, self.c_step)
-        )
+        coords = self.functions.get_coordinates_at_time_step(self.instance, self.c_step)
+        self.coord_trajectory.append(coords)
+        self.action_trajectory.append(np.array(action_vec))
 
         self.coord_trajectory_set[self.inst_id] = self.coord_trajectory
-        self.trajectory_set[self.inst_id] = self.trajectory
+        self.action_trajectory_set[self.inst_id] = self.action_trajectory
 
         if self.realistic_trajectory:
             self.derivative = self._calculate_derivative(self.coord_trajectory)
         else:
-            self.derivative = self._calculate_derivative(self.trajectory)
+            self.derivative = self._calculate_derivative(self.action_trajectory)
 
         self.derivative_set[self.inst_id] = self.derivative
 
@@ -208,7 +178,7 @@ class GeometricEnv(AbstractEnv):
         """
         super(GeometricEnv, self).reset_()
 
-        self.trajectory = self.trajectory_set.get(
+        self.action_trajectory = self.action_trajectory_set.get(
             self.inst_id, [np.zeros(self.n_actions)]
         )
         self.coord_trajectory = self.coord_trajectory_set.get(
@@ -239,14 +209,14 @@ class GeometricEnv(AbstractEnv):
         function_names = []
 
         for dim, function_info in enumerate(self.instance):
-            coordinates[dim] = self._calculate_function_value(
+            coordinates[dim] = self.functions.calculate_function_value(
                 self.c_step, function_info
             )
             function_names.append(function_info[1])
 
         # TODO: - add correlation for all changes based on trajectories
         if self.correlation_active:
-            self._add_correlation_between_dimensions(coordinates)
+            pass
 
         # map action values to their interval mean
         mapping_list = [self.action_interval_mapping[name] for name in function_names]
@@ -303,187 +273,6 @@ class GeometricEnv(AbstractEnv):
         """
         return True
 
-    def _sigmoid(self, t: float, scaling: float, inflection: float):
-        """Simple sigmoid function"""
-        return 1 / (1 + np.exp(-scaling * (t - inflection)))
-
-    def _linear(self, t: float, a: float, b: float):
-        """Linear function"""
-        return a * t + b
-
-    def _polynom(self, t: float, coeff_list: List[float]):
-        """Polynomial function. Dimension depends on length of coefficient list."""
-        pol_value = 0
-
-        for dim, coeff in enumerate(coeff_list):
-            pol_value += coeff * t ** dim
-
-        return pol_value
-
-    def _logarithmic(self, t: float, a: float):
-        """Logarithmic function"""
-        if t != 0:
-            return a * np.log(t)
-        else:
-            return self.max_function_value
-
-    def _exponential(self, t: float, a: int):
-        """Exponential function"""
-        return a * np.exp(t)
-
-    def _constant(self, c: float):
-        """Constant function"""
-        return c
-
-    def _sinus(self, t: float, scale: float):
-        """Sinus function"""
-        return np.sin(scale * t)
-
-    def _calculate_norm_value(self):
-        """
-        Norm Functions to Intervall between -1 and 1
-        """
-        for key, instance in self.instance_set.items():
-            instance_values = self.get_coordinates(instance).transpose()
-            # TODO: - add correlation and return max_value
-
-            for dim, function_values in enumerate(instance_values):
-
-                if abs(min(function_values)) > max(function_values):
-                    norm_factor = abs(min(function_values))
-                else:
-                    norm_factor = max(function_values)
-
-                # set first item of every function_list in instance as norm factor
-                self.instance_set[key][dim][0] = norm_factor
-
-        self._calculate_norm = False
-
-    def _calculate_function_value(self, time_step: int, function_infos: List) -> float:
-        """
-        Call different functions with their speicifc parameters and norm them.
-
-        Parameters
-        ----------
-        function_infos : List
-            Consists of function name and the coefficients
-        time_step: int
-            time step for each function
-        calculate_norm : bool, optional
-            True if norm gets calculated, by default False
-
-        Returns
-        -------
-        float
-            coordinate in dimension of function
-        """
-        norm_value = function_infos[0] if not self._calculate_norm else 1
-        function_name = function_infos[1]
-        coefficients = function_infos[2:]
-
-        function_value = 0
-
-        if "sigmoid" == function_name:
-            function_value = self._sigmoid(time_step, coefficients[0], coefficients[1])
-
-        elif "linear" == function_name:
-            function_value = self._linear(time_step, coefficients[0], coefficients[1])
-
-        elif "constant" == function_name:
-            function_value = self._constant(coefficients[0])
-
-        elif "exponential" == function_name:
-            function_value = self._exponential(time_step, coefficients[0])
-
-        elif "logarithmic" == function_name:
-            function_value = self._logarithmic(time_step, coefficients[0])
-
-        elif "polynomial" in function_name:
-            function_value = self._polynom(time_step, coefficients)
-
-        elif "sinus" in function_name:
-            function_value = self._sinus(time_step, coefficients[0])
-
-        function_value = max(function_value, -self.max_function_value)
-        return min(function_value, self.max_function_value) / norm_value
-
-    def _calculate_derivative(self, trajectory: List) -> np.array:
-        """
-        Calculate derivatives of each dimension, based on trajectories.
-
-        Parameters
-        ----------
-        trajectory: List
-            List of actions or coordinates already taken
-
-        Returns
-        -------
-        np.array
-            derivatives for each dimension
-        """
-        if self.c_step > 1:
-            upper_bound = self.c_step + 1
-            lower_bound = max(upper_bound - self.derivative_interval, 1)
-
-            derrivative = np.zeros(self.n_actions)
-            for step in range(lower_bound, upper_bound):
-                der = np.subtract(
-                    np.array(trajectory[step], dtype=np.float),
-                    np.array(trajectory[step - 1], dtype=np.float),
-                )
-                derrivative = np.add(derrivative, der)
-
-            derrivative /= upper_bound - lower_bound
-
-        elif self.c_step == 1:
-            derrivative = np.subtract(
-                np.array(trajectory[self.c_step], dtype=np.float),
-                np.array(trajectory[self.c_step - 1], dtype=np.float),
-            )
-
-        else:
-            derrivative = np.zeros(self.n_actions)
-
-        return derrivative
-
-    def _get_optimal_policy_at_time_step(
-        self, instance: List, time_step: int
-    ) -> np.array:
-        """calculate optimal policy at time_step"""
-        value_array = np.zeros(self.n_actions)
-        for index, function_info in enumerate(instance):
-            value_array[index] = self._calculate_function_value(
-                time_step, function_info
-            )
-
-        return value_array
-
-    def _add_correlation_between_dimensions(
-        self, current_values: np.ndarray, prev_values: np.ndarray, current_depth: int
-    ):
-        """
-        Adds correlation between dimensions.
-        Correlation table holds numbers between -1 and 1.
-        e.g. correlation_table[0][2] = 0.5 if dimension 1 changes dimension 3 changes about 50% of dimension one
-        Call function recursively till max_depth is reached
-
-        Parameters
-        ----------
-        correlation_table : np.array
-            table that holds all values of correlation between dimensions [n,n]
-        """
-        if current_depth < self.correlation_depth:
-            # do value update
-            corr_values = 1
-            self._add_correlation_between_dimensions(
-                current_values, corr_values, current_depth + 1
-            )
-        else:
-            corr_values = self._apply_correlation_update()
-
-    def _apply_correlation_update(self):
-        pass
-
     def render_dimensions(self, dimensions: List, absolute_path: str):
         """
         Multiplot for specific dimensions of benchmark with policy actions.
@@ -493,7 +282,7 @@ class GeometricEnv(AbstractEnv):
         dimensions : List
             List of dimensions that get plotted
         """
-        coordinates = self.get_coordinates().transpose()
+        coordinates = self.functions.get_coordinates().transpose()
         instance = self.instance
 
         fig, axes = plt.subplots(
@@ -529,7 +318,7 @@ class GeometricEnv(AbstractEnv):
         """
         assert len(dimensions) == 2
 
-        coordinates = self.get_coordinates().transpose()
+        coordinates = self.functions.get_coordinates().transpose()
 
         fig = plt.figure(figsize=(10, 10))
         ax = plt.axes(projection="3d")
@@ -551,19 +340,45 @@ class GeometricEnv(AbstractEnv):
 
 
 class Functions:
-    def __init__(self) -> None:
+    def __init__(self, n_steps: int, n_actions: int) -> None:
         self.norm_calculated = False
+        self.correlation = False
+        self.n_steps = n_steps
+        self.n_actions = n_actions
         self._calculate_norm_value()
 
-    def get_optimal_policy_at_time_step(
-        self, instance: List, time_step: int
-    ) -> np.array:
+    def get_coordinates(self, instance: List = None) -> List[np.array]:
+        """
+        Calculates coordinates for instance over all time_steps
+
+        Parameters
+        ----------
+        instance : List, optional
+            Instance that holds information about functions, by default None
+
+        Returns
+        -------
+        List[np.array]
+            Index of List refers to time step
+        """
+        if not instance:
+            instance = self.instance
+
+        optimal_policy_coord = np.zeros((self.n_steps, self.n_actions))
+
+        for time_step in range(self.n_steps):
+            # TODO: apply correlation
+            optimal_policy_coord[time_step, :] = self.get_coordinates_at_time_step(
+                instance, time_step
+            )
+
+        return optimal_policy_coord
+
+    def get_coordinates_at_time_step(self, instance: List, time_step: int) -> np.array:
         """calculate optimal policy at time_step"""
         value_array = np.zeros(self.n_actions)
         for index, function_info in enumerate(instance):
-            value_array[index] = self._calculate_function_value(
-                time_step, function_info
-            )
+            value_array[index] = self.calculate_function_value(time_step, function_info)
 
         return value_array
 
@@ -615,7 +430,7 @@ class Functions:
         function_value = max(function_value, -self.max_function_value)
         return min(function_value, self.max_function_value) / norm_value
 
-    def _calculate_derivative(self, trajectory: List) -> np.array:
+    def calculate_derivative(self, trajectory: List) -> np.array:
         """
         Calculate derivatives of each dimension, based on trajectories.
 
