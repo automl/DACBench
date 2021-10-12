84ae28915a1af91a49423ba64141fb7061eb7298
Rasmus von Glahn <rasmusvong@gmx.de>
2021-10-12 15:13:36 +0200

    fix: add more realistic values for samples

--- dacbench/instance_sets/geometric/SampleGeometricInstances.py
+++ dacbench/instance_sets/geometric/SampleGeometricInstances.py
@@ -1,3 +1,4 @@
+from __future__ import generators
 from typing import Dict
 import numpy as np
 import os
@@ -10,7 +11,6 @@ FUNCTION_CONFIG = {
     "linear": 1,
     "polynomial2D": 1,
     "polynomial3D": 1,
-    "polynomial7D": 1,
     "exponential": 1,
     "logarithmic": 1,
     "constant": 1,
@@ -23,7 +23,6 @@ FUNCTION_PARAMETER_NUMBERS = {
     "linear": 2,
     "polynomial2D": 3,
     "polynomial3D": 4,
-    "polynomial7D": 8,
     "exponential": 1,
     "logarithmic": 1,
     "constant": 1,
@@ -48,8 +47,7 @@ def save_geometric_instances(filename: str, config: Dict = FUNCTION_CONFIG):
     csv_path = os.path.join(FILE_PATH, filename)
 
     if os.path.exists(csv_path):
-        # os.remove(csv_path)
-        pass
+        os.remove(csv_path)
 
     with open(csv_path, "a") as fh:
         id_string = (
@@ -87,16 +85,48 @@ def _create_csv_string(index, func_name: str) -> str:
 
     csv_string = str(index) + "," + func_name
 
+    if func_name == "sigmoid":
+        value_generator = sample_sigmoid_value()
+    elif "polynomial" in func_name:
+        value_generator = sample_polynomial_value()
+
     for i in range(max_count):
+
         if i < count:
-            csv_string += "," + str(
-                np.round(np.random.uniform(low=-10.0, high=10.0), 3)
-            )
+
+            if func_name == "sinus":
+                value = sample_sinus_value()
+            elif func_name == "sigmoid":
+                value = next(value_generator)
+            elif "polynomial" in func_name:
+                value = next(value_generator)
+            else:
+                value = np.round(np.random.uniform(low=-10.0, high=10.0), 1)
+
+            csv_string += "," + str(value)
         else:
             csv_string += ",0"
 
     csv_string += "\n"
     return csv_string
+
+
+def sample_sinus_value():
+    return np.round(np.random.uniform(low=0.5, high=2.0), 1)
+
+
+def sample_sigmoid_value():
+    scale = np.round(np.random.uniform(low=0.1, high=4.0), 1)
+    yield scale
+    infliction = np.round(np.random.uniform(low=0, high=10), 1)
+    yield infliction
+
+
+def sample_polynomial_value():
+    start_value = np.round(np.random.uniform(low=50, high=100), 1)
+
+    for n in range(1, 999):
+        yield np.power(start_value, 1 / n)
 
 
 if __name__ == "__main__":
