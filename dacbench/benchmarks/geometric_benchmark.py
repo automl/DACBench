fix: chnage distance calculation in reward fucntion

dacbench/benchmarks/geometric_benchmark.py
@@ -33,7 +33,7 @@ GEOMETRIC_DEFAULTS = objdict(
         # if action_values_variable True action_value_mapping will be used instead of action_value_default to define action values
         # action_value_mapping defines number of action values for differnet functions
         # sigmoid is split in 3 actions, polynomial3D in 7 etc.
-        "action_values_variable": False,
+        "action_values_variable": True,
         "action_value_mapping": {
             "sigmoid": 3,
             "linear": 3,
@@ -221,7 +221,9 @@ class GeometricBenchmark(AbstractBenchmark):
 
                     action_interval.append(middle)
 
-                self.config.action_interval_mapping[function_name] = action_interval
+                self.config.action_interval_mapping[function_name] = np.round(
+                    action_interval, 3
+                )
 
         self.config.action_values = values
         self.config.action_space_args = [int(np.prod(values))]
@@ -262,9 +264,10 @@ if __name__ == "__main__":
     env = geo_bench.get_environment()
 
     opt_policy = env.get_optimal_policy()
-    env.render_dimensions([0, 1, 2, 3, 4, 5, 6], "/home/vonglahn/tmp")
+    env.render_dimensions([0, 1, 2, 3, 4, 7, 8], "/home/vonglahn/tmp")
     # env.render_3d_dimensions([1, 3], "/home/vonglahn/tmp")
     env.reset()
 
     for step in range(env.n_steps):
-        env.step(3)
+        state, reward, done, info = env.step(np.random.randint(env.action_space.n))
+        print(reward)

dacbench/envs/geometric.py
@@ -198,8 +198,9 @@ class GeometricEnv(AbstractEnv):
             self.inst_id, [np.zeros(self.n_actions)]
         )
 
-        # TODO: is 0 correct or vec of zeros?
-        self.derivative = self.derivative_set.get(self.inst_id, 0)
+        self.derivative = self.derivative_set.get(
+            self.inst_id, np.zeros(self.n_actions)
+        )
         self._prev_state = None
 
         return self.get_state(self)
@@ -230,9 +231,8 @@ class GeometricEnv(AbstractEnv):
         euclidean_dist = np.linalg.norm(action_intervall - coordinates)
 
         # norm reward to (0, 1)
-        # TODO: fix errors
-        highest_coords = np.ones(max(self.action_vals))
-        lowest_actions = np.full((max(self.action_vals)), np.min(mapping_list))
+        highest_coords = np.ones(self.n_actions)
+        lowest_actions = np.ones(self.n_actions) * -1
         max_dist = np.linalg.norm(highest_coords - lowest_actions)
 
         reward = 1 - (euclidean_dist / max_dist)
@@ -550,7 +550,7 @@ class Functions:
         elif "sinus" in function_name:
             function_value = self._sinus(time_step, coefficients[0])
 
-        return function_value / norm_value
+        return np.round(function_value / norm_value, 5)
 
     def _add_correlation(self, value_array: np.ndarray, time_step: int):
         """