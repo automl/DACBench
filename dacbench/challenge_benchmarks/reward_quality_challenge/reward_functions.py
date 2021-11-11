f1c1e8ae1761bdbebe5f6bd00e896ca7e0c29535
Rasmus von Glahn <rasmusvong@gmx.de>
2021-11-11 10:18:17 +0100

    fix: manhattan

--- dacbench/challenge_benchmarks/reward_quality_challenge/reward_functions.py
+++ dacbench/challenge_benchmarks/reward_quality_challenge/reward_functions.py
@@ -62,6 +62,19 @@ def manhattan_distance_reward_geometric(self):
     return abs(reward)
 
 
+def quadratic_manhattan_distance_reward_geometric(self):
+    def manhattan(a, b):
+        return sum(abs(val1 - val2) for val1, val2 in zip(a, b))
+
+    coordinates, action_intervall, highest_coords, lowest_actions = self._pre_reward()
+    manhattan_dist = manhattan(coordinates, action_intervall)
+
+    max_dist = manhattan(lowest_actions, highest_coords)
+    reward = (1 - (manhattan_dist / max_dist)) ** 2
+
+    return abs(reward)
+
+
 def quadratic_euclidean_distance_reward_geometric(self):
     coords, action_coords, highest_coords, lowest_actions = self._pre_reward()
     euclidean_dist = np.linalg.norm(action_coords - coords)
