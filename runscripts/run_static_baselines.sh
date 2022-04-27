87a234f47ea5dda66d1c06a5e1f44547a0bf3001
Theresa Eimer <eimer@tnt.uni-hannover.de>
2022-04-27 11:51:00 +0200

    removed outdated runscripts dir

--- runscripts/run_static_baselines.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --partition=cpu_cluster
-#SBATCH --job-name=dacbenchlines
-#SBATCH --time=72:00:00
-#SBATCH --mem-per-cpu=8000M
-
-python dacbench/run_baselines.py --outdir baselines --static --num_episodes 1000 --benchmarks $1
