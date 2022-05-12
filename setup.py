e07affe2bbc331e61f9274ae30b4e2a016e03d5a
Areeb Ahmad <areebfarhan@outlook.com>
2022-05-12 23:49:29 +0200

    cont

--- setup.py
+++ setup.py
@@ -5,6 +5,16 @@ import os
 
 def other_requirements():
     other_requirements = {}
+    for file in os.listdir('./container/other_requirements'):
+        with open(f'./container/other_requirements/{file}', encoding='utf-8') as rq:
+            requirements = json.load(rq)
+            other_requirements.update(requirements)
+            return other_requirements
+
+def file_rd(file_name):
+    with open(file_name, enconding='utf-8') as rq:
+        text = rq.read()
+        return text
 
 
 setuptools.setup()
