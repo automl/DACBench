278c7beb0694be7642c7be1bfc725c7f99a806c9
Areeb Ahmad <areebfarhan@outlook.com>
2022-05-15 03:07:52 +0200

    cont

--- setup.py
+++ setup.py
@@ -11,12 +11,44 @@ def get_other_requirements():
             other_requirements.update(requirements)
             return other_requirements
 
+def file_rd(file_name):
+    with open(file_name, enconding='utf-8') as rq:
+        text = rq.read()
+        return text
+
 
 setup(
+    name='DACBench',
+    version = '0.0.1',
+    author_email = 'eimer@tnt.uni-hannover.de',
+    author = 'Theresa Eimer',
+    url = 'https://www.automl.org/automl/dacbench/',
+    project_urls={
+        'Documentation' : 'https://dacbench.readthedocs.io/en/latest/',
+    'Project Page' : 'https://www.tnt.uni-hannover.de/en/project/dacbench/'
+    },
+    license = 'Apache 2.0',
 
+    packages=find_packages(exclude=['tests', 'examples', 'dacbench.wrappers.*', 'dacbench.envs.fast-downward/*']),
 
-    packages=find_packages(exclude=['tests', 'examples', 'dacbench.wrappers.*', 'dacbench.envs.fast-downward/*'])
+    keywords = [
+    'DAC',
+    'Dynamic Algorithm Configuration',
+    'HPO'
+    ],
 
-
+    classifiers = [
+     'Programming Language :: Python :: 3',
+     'Natural Language :: English',
+     'Environment :: Console',
+     'Intended Audience :: Developers',
+     'Intended Audience :: Education',
+     'Intended Audience :: Science/Research',
+     'License :: OSI Approved :: Apache Software License',
+     'Operating System :: POSIX :: Linux',
+     'Topic :: Scientific/Engineering :: Artificial Intelligence',
+     'Topic :: Scientific/Engineering',
+     'Topic :: Software Development',
+    ]
 
 )
