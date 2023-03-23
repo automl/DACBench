311719131243480347949adc703034623c6a235c
Theresa Eimer <eimer@tnt.uni-hannover.de>
2023-03-23 16:35:51 +0100

    ignore builds

--- build/lib/dacbench/envs/policies/optimal_fd.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-def get_optimum(env, state):
-    instance = env.get_instance()[:-12] + "optimal.json"
-    with open(instance, "r+") as fp:
-        optimal = json.load(fp)
-    return optimal[env.c_step]
