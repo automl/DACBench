4296f6382943a6ed88a7fa5a9babe679f46e2fda
Theresa Eimer <eimer@tnt.uni-hannover.de>
2023-03-24 17:12:58 +0100

    branch cleanup

--- dacbench/envs/policies/csa_cma.py
+++
@@ -1,7 +0,0 @@
-def csa(env, state):
-    u = env.es.sigma
-    hsig = env.es.adapt_sigma.hsig(env.es)
-    env.es.hsig = hsig
-    delta = env.es.adapt_sigma.update2(env.es, function_values=env.cur_obj_val)
-    u *= delta
-    return u
