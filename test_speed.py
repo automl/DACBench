afed3ea3f6acf2120d0fa3a1278483f18eee059c
Theresa Eimer <eimer@tnt.uni-hannover.de>
2023-05-08 12:00:28 +0200

    playing around with other envs

--- test_speed.py
+++ test_speed.py
@@ -18,13 +18,14 @@ config_cartpole =  {
             "activation": "tanh",
             "hidden_size": 64,
             "env_name": "CartPole-v1", 
-            "num_eval_episodes": 10}
+            "num_eval_episodes": 10,
+            "env_framework": "gymnax"}
 
 config_cartpole_full =  {
             "lr": 2.5e-4,
             "num_envs": 4,
             "num_steps": 128,
-            "total_timesteps": 5e5,
+            "total_timesteps": 1e6,
             "update_epochs": 4,
             "num_minibatches": 4,
             "gamma": 0.99,
@@ -36,7 +37,9 @@ config_cartpole_full =  {
             "activation": "tanh",
             "hidden_size": 64,
             "env_name": "CartPole-v1", 
-            "num_eval_episodes": 10}
+            "num_eval_episodes": 10,
+            "env_framework": "gym",
+            "checkpoint_dir": "test_cartpole"}
 
 config_minatar_breakout =  {
             "lr": 5e-4,
@@ -61,7 +64,7 @@ config_minatar_full =  {
             "lr": 5e-4,
             "num_envs": 64,
             "num_steps": 128,
-            "total_timesteps": 1e7,
+            "total_timesteps": 25e6,
             "update_epochs": 4,
             "num_minibatches": 8,
             "gamma": 0.999,
@@ -72,62 +75,68 @@ config_minatar_full =  {
             "max_grad_norm": 0.5,
             "activation": "tanh",
             "hidden_size": 256,
-            "env_name": "BreakoutDeterministic-v4", 
+            "env_name": "procgen:procgen-coinrun-v0", 
             "num_eval_episodes": 10,
-            "env_framework": "gym"}
+            "env_framework": "gym",
+            "track_traj": False}
 
-bench = AutoRLBenchmark()
-bench.config.instance_set = {0: config_cartpole_full}
-bench.config.env_framework = "gymnax"
-env = bench.get_environment()
-env.reset()
-start = time.time()
-_, reward, _, _, _ = env.step({})
-end = time.time() - start
-print(f"CartPole took {np.round(end, decimals=2)}s for {reward} points in 500000 steps in the gymnax version.")
+# bench = AutoRLBenchmark()
+# bench.config.instance_set = {0: config_cartpole_full}
+# #bench.config.checkpoint = True
+# #bench.config.checkpoint_dir = "test_cartpole"
+# bench.config.cutoff = 1
+# env = bench.get_environment()
+# env.reset()
+# start = time.time()
+# _, reward, te, tr, _ = env.step({})
+# end = time.time() - start
+# print(te or tr)
+# print(f"CartPole took {np.round(end, decimals=2)}s for {reward} points in 1M steps in the gymnax version (including saving all data).")
 
-bench = AutoRLBenchmark()
-bench.config.instance_set = {0: config_cartpole_full}
-bench.config.env_framework = "gym"
-env = bench.get_environment()
-env.reset()
-start = time.time()
-_, reward, _, _, _ = env.step({})
-end = time.time() - start
-print(f"CartPole took {np.round(end, decimals=2)}s for {reward} points in 500000 steps in the gym version.")
+# bench = AutoRLBenchmark()
+# bench.config.instance_set = {0: config_cartpole_full}
+# bench.config.env_framework = "gym"
+# env = bench.get_environment()
+# env.reset()
+# start = time.time()
+# _, reward, _, _, _ = env.step({})
+# end = time.time() - start
+# print(f"CartPole took {np.round(end, decimals=2)}s for {reward} points in 500000 steps in the gym version.")
 
 
-bench = AutoRLBenchmark()
-bench.config.instance_set = {0: config_cartpole}
-env = bench.get_environment()
-env.reset()
-start = time.time()
-for i in range(10):#563):
-    _, reward, _, _, _ = env.step({})
-    if i == 0:
-        end = time.time() - start
-        print(f"CartPole took {np.round(end, decimals=2)}s for {reward} points in 64 steps.")
-end = time.time() - start
-print(f"CartPole took {np.round(end, decimals=2)}s for {reward} points in 100000 steps in 10 intervals of 10000 steps.")
+# bench = AutoRLBenchmark()
+# bench.config.instance_set = {0: config_cartpole}
+# env = bench.get_environment()
+# env.reset()
+# start = time.time()
+# for i in range(10):#563):
+#     _, reward, _, _, _ = env.step({})
+#     if i == 0:
+#         end = time.time() - start
+#         print(f"CartPole took {np.round(end, decimals=2)}s for {reward} points in 64 steps.")
+# end = time.time() - start
+# print(f"CartPole took {np.round(end, decimals=2)}s for {reward} points in 100000 steps in 10 intervals of 10000 steps.")
+
+
 
 bench = AutoRLBenchmark()
 bench.config.instance_set = {0: config_minatar_full}
 env = bench.get_environment()
 env.reset()
 start = time.time()
-_, reward, _, _, _ = env.step({})
-end = time.time() - start
-print(f"Breakout took {np.round(end, decimals=2)}s for {reward} points in 10M steps.")
-
-bench = AutoRLBenchmark()
-bench.config.instance_set = {0: config_minatar_breakout}
-env = bench.get_environment()
-env.reset()
-start = time.time()
-for i in range(10):#000):
+for i in range(int(1)):
     _, reward, _, _, _ = env.step({})
     if i == 0:
         end = time.time() - start
-        print(f"Breakout took {np.round(end, decimals=2)}s for {reward} points in 1000 steps.")
+        print(f"ProcGen took {np.round(end, decimals=2)}s for {reward} points in 1.000.000 steps.")
 end = time.time() - start
-print(f"Breakout took {np.round(end, decimals=2)}s for {reward} points in 10M steps in 10 intervals of 1e6 steps.")
+print(f"ProcGen took {np.round(end, decimals=2)}s for {reward} points in 25M steps in 25 intervals of 1.000.000 steps.")
+
+# bench = AutoRLBenchmark()
+# bench.config.instance_set = {0: config_minatar_full}
+# env = bench.get_environment()
+# env.reset()
+# start = time.time()
+# _, reward, _, _, _ = env.step({})
+# end = time.time() - start
+# print(f"Breakout took {np.round(end, decimals=2)}s for {reward} points in 10M steps.")
